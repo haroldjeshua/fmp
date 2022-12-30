e42637ccf0d90f63858b278bc1712423a0158df2
Harold Varde <harvardii15@gmail.com>
2022-12-30 23:45:25 +0800

    MOD: theme script

--- alligator-io/main.js
+++ alligator-io/main.js
@@ -5,11 +5,45 @@ const darkBtn = document.querySelector("#dark");
 const solartBtn = document.querySelector("#solar");
 const body = document.body;
 
+// Apply theme on reload
+const theme = localStorage.getItem("theme");
+const isSolar = localStorage.getItem("isSolar");
+
+if (theme) {
+  body.classList.add(theme);
+  isSolar && body.classList.add("solar");
+}
+
 // Button handlers
 lightBtn.addEventListener("click", () => {
-  body.classList.replace("light", "dark");
+  body.classList.replace("dark", "light");
+
+  localStorage.setItem("theme", "light");
 });
 
 darkBtn.addEventListener("click", () => {
-  body.classList.replace("dark", "light");
+  body.classList.replace("light", "dark");
+
+  localStorage.setItem("theme", "dark");
+});
+
+solartBtn.addEventListener("click", () => {
+  if (body.classList.contains("solar")) {
+    body.classList.remove("solar");
+    solartBtn.getElementsByClassName.cssText = `
+      --bg-solar: var(--yellow)
+    `;
+
+    solartBtn.innerText = "solarize";
+    localStorage.removeItem("isSolar");
+  } else {
+    solartBtn.style.cssText = `
+      --bg-solar: white;
+    `;
+
+    body.classList.add("solar");
+    solartBtn.innerText = "normalize";
+
+    localStorage.setItem("isSolar", true);
+  }
 });
