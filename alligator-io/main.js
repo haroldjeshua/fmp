0e03529e4509fa879ec15a4b1f1cb3a5e5c54f88
Harold Varde <harvardii15@gmail.com>
2022-12-30 23:29:12 +0800

    MOD: styling & script

--- alligator-io/main.js
+++ alligator-io/main.js
@@ -1,1 +1,15 @@
 import "./style.css";
+
+const lightBtn = document.querySelector("#light");
+const darkBtn = document.querySelector("#dark");
+const solartBtn = document.querySelector("#solar");
+const body = document.body;
+
+// Button handlers
+lightBtn.addEventListener("click", () => {
+  body.classList.replace("light", "dark");
+});
+
+darkBtn.addEventListener("click", () => {
+  body.classList.replace("dark", "light");
+});
