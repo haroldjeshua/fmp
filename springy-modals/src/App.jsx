3c6f17c36e38960ecf14eb5e6a6066eb0553130f
Harold Varde <harvardii15@gmail.com>
2023-01-01 23:54:59 +0800

    ADD: input for notif + modal drag

--- springy-modals/src/App.jsx
+++ springy-modals/src/App.jsx
@@ -10,28 +10,44 @@ function App() {
   const open = () => setModalOpen(true);
 
   return (
-    <div className="App">
-      <motion.button
-        whileHover={{ scale: 1.1 }}
-        whileTap={{ scale: 0.95 }}
-        className="save-button"
-        onClick={() => (modalOpen ? close() : open())}
-      >
-        Open Modal
-      </motion.button>
+    <>
+      <motion.main>
+        <Header />
+        <SubHeader />
 
-      <AnimatePresence
-        // Disable any initial animations on children that are present when the component is first rendered
-        initial={false}
-        // Only render one comp at a time. The exiting component will finish its exit animation before component is rendered
-        mode="wait"
-        // Fires when all exiting nodes have completed animating out
-        onExitComplete={() => null}
-      >
-        {modalOpen && <Modal modalOpen={modalOpen} handleClose={close} />}
-      </AnimatePresence>
-    </div>
+        <motion.button
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.95 }}
+          className="save-button"
+          onClick={() => (modalOpen ? close() : open())}
+        >
+          Open Modal
+        </motion.button>
+
+        <AnimatePresence
+          // Disable any initial animations on children that are present when the component is first rendered
+          initial={false}
+          // Only render one comp at a time. The exiting component will finish its exit animation before component is rendered
+          mode="wait"
+          // Fires when all exiting nodes have completed animating out
+          onExitComplete={() => null}
+        >
+          {modalOpen && <Modal modalOpen={modalOpen} handleClose={close} />}
+        </AnimatePresence>
+      </motion.main>
+    </>
   );
 }
+
+const Header = () => (
+  <motion.h1>
+    Modal Animation w/ <span className="light-blue">React</span> +{" "}
+    <span className="pink">Framer Motion</span>
+  </motion.h1>
+);
+
+const SubHeader = ({ text }) => (
+  <motion.h2 className="sub-header">{text}</motion.h2>
+);
 
 export default App;
