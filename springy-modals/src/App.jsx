b4c4a1076e003e72bf254c7b5336ae3ea63ddcd6
Harold Varde <harvardii15@gmail.com>
2023-01-01 23:13:33 +0800

    ADD: modal comp + animation

--- springy-modals/src/App.jsx
+++ springy-modals/src/App.jsx
@@ -1,17 +1,26 @@
 import { motion } from "framer-motion";
+import { useState } from "react";
 import "./App.css";
+import Modal from "./components/Modal";
 
 function App() {
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const close = () => setModalOpen(false);
+  const open = () => setModalOpen(true);
+
   return (
     <div className="App">
       <motion.button
         whileHover={{ scale: 1.1 }}
         whileTap={{ scale: 0.95 }}
         className="save-button"
-        onClick={() => null}
+        onClick={() => (modalOpen ? close() : open())}
       >
         Open Modal
       </motion.button>
+
+      {modalOpen && <Modal modalOpen={modalOpen} handleClose={close} />}
     </div>
   );
 }
--- springy-modals/src/components/Modal.jsx
+++ springy-modals/src/components/Modal.jsx
@@ -1,8 +1,42 @@
 import { motion } from "framer-motion";
 import Backdrop from "./Backdrop";
 
+const dropIn = {
+  hidden: {
+    y: "-100vh",
+    opacity: 0,
+  },
+  visible: {
+    y: "0",
+    opacity: 1,
+    transition: {
+      duration: 0.1,
+      type: "spring",
+      damping: 0,
+      stiffness: 500,
+    },
+  },
+  exit: {
+    y: "100vh",
+    opacity: 0,
+  },
+};
+
 const Modal = ({ handleClose, text }) => {
-  return <Backdrop onClick={handleClose}></Backdrop>;
+  return (
+    <Backdrop onClick={handleClose}>
+      <motion.div
+        onClick={(e) => e.stopPropagation}
+        className="modal orange-gradient"
+        variants={dropIn}
+        initial="hidden"
+        animate="visible"
+        exit="exit"
+      >
+        test
+      </motion.div>
+    </Backdrop>
+  );
 };
 
 export default Modal;
