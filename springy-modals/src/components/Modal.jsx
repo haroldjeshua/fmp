e9399357775c5195a78e0e5c147c70c4b518b0bf
Harold Varde <harvardii15@gmail.com>
2023-01-02 00:47:28 +0800

    MOD: add select animation options for modal

--- springy-modals/src/components/Modal.jsx
+++ springy-modals/src/components/Modal.jsx
@@ -1,5 +1,7 @@
 import { motion } from "framer-motion";
 import Backdrop from "./Backdrop";
+import { useEffect } from "react";
+import { stateLogger } from "../stateLogger";
 
 const dropIn = {
   hidden: {
@@ -22,23 +24,211 @@ const dropIn = {
   },
 };
 
-const Modal = ({ handleClose, text }) => {
+const flip = {
+  hidden: {
+    transform: "scale(0) rotateX(-360deg)",
+    opacity: 0,
+    transition: {
+      delay: 0.3,
+    },
+  },
+  visible: {
+    transform: " scale(1) rotateX(0deg)",
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+    },
+  },
+  exit: {
+    transform: "scale(0) rotateX(360deg)",
+    opacity: 0,
+    transition: {
+      duration: 0.5,
+    },
+  },
+};
+
+const newspaper = {
+  hidden: {
+    transform: "scale(0) rotate(720deg)",
+    opacity: 0,
+    transition: {
+      delay: 0.3,
+    },
+  },
+  visible: {
+    transform: " scale(1) rotate(0deg)",
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+    },
+  },
+  exit: {
+    transform: "scale(0) rotate(-720deg)",
+    opacity: 0,
+    transition: {
+      duration: 0.3,
+    },
+  },
+};
+
+const badSuspension = {
+  hidden: {
+    y: "-100vh",
+    opacity: 0,
+    transform: "scale(0) rotateX(-360deg)",
+  },
+  visible: {
+    y: "-25vh",
+    opacity: 1,
+    transition: {
+      duration: 0.2,
+      type: "spring",
+      damping: 15,
+      stiffness: 500,
+    },
+  },
+  exit: {
+    y: "-100vh",
+    opacity: 0,
+  },
+};
+
+const gifYouUp = {
+  hidden: {
+    opacity: 0,
+    scale: 0,
+  },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      duration: 0.2,
+      ease: "easeIn",
+    },
+  },
+  exit: {
+    opacity: 0,
+    scale: 0,
+    transition: {
+      duration: 0.15,
+      ease: "easeOut",
+    },
+  },
+};
+
+const Modal = ({ handleClose, text, type }) => {
+  useEffect(() => {
+    stateLogger("Modal", true);
+    return () => stateLogger("Modal", false);
+  }, []);
+
   return (
     <Backdrop onClick={handleClose}>
-      <motion.div
-        drag
-        onClick={(e) => e.stopPropagation}
-        className="modal purple-gradient"
-        variants={dropIn}
-        initial="hidden"
-        animate="visible"
-        exit="exit"
-      >
-        {/* <ModalText text={text} />
-        <ModalButton onClick={handleClose} label="Close" /> */}
-        <p>{text}</p>
-        <button onClick={handleClose}>Close</button>
-      </motion.div>
+      {type === "dropIn" && (
+        <motion.div
+          onClick={(e) => e.stopPropagation()} // Prevent click from closing modal
+          className="modal orange-gradient"
+          variants={dropIn}
+          initial="hidden"
+          animate="visible"
+          exit="exit"
+        >
+          <ModalText text={text} />
+          <ModalButton onClick={handleClose} label="Close" />
+        </motion.div>
+      )}
+
+      {type === "flip" && (
+        <motion.div
+          onClick={(e) => e.stopPropagation()}
+          className="modal  orange-gradient"
+          variants={flip}
+          initial="hidden"
+          animate="visible"
+          exit="exit"
+        >
+          <ModalText text={text} />
+          <ModalButton onClick={handleClose} label="Close" />
+        </motion.div>
+      )}
+
+      {type === "newspaper" && (
+        <motion.div
+          onClick={(e) => e.stopPropagation()}
+          className="modal orange-gradient"
+          variants={newspaper}
+          initial="hidden"
+          animate="visible"
+          exit="exit"
+        >
+          <ModalText text={text} />
+          <ModalButton onClick={handleClose} label="Close" />
+        </motion.div>
+      )}
+
+      {type === "badSuspension" && (
+        <motion.div
+          onClick={(e) => e.stopPropagation()}
+          className="modal orange-gradient"
+          variants={badSuspension}
+          initial="hidden"
+          animate="visible"
+          exit="exit"
+        >
+          <ModalText text={text} />
+
+          <ModalButton onClick={handleClose} label="Close" />
+        </motion.div>
+      )}
+
+      {type === "gifYouUp" && (
+        <motion.div
+          className="modal"
+          onClick={(e) => e.stopPropagation()}
+          style={{
+            padding: 0,
+            height: "auto",
+            width: "auto",
+            display: "flex",
+            justifyContent: "center",
+          }}
+          variants={gifYouUp}
+          initial="hidden"
+          animate="visible"
+          exit="exit"
+        >
+          <h3
+            style={{
+              color: "#ffaa00",
+              textTransform: "none",
+              fontWeight: 400,
+              margin: "auto auto auto 0",
+              fontFamily: "Montserrat",
+              fontSize: "150%",
+            }}
+          >
+            Tap x2 to close
+          </h3>
+          <motion.img
+            alt=""
+            onDoubleClick={handleClose}
+            drag
+            // src="https://i.giphy.com/media/O5ac76MtFGPHG/giphy.gif"
+            // src="https://i.giphy.com/media/jmS6YojdAaYw5z1LHi/giphy.gif"
+            src="https://i.giphy.com/media/hhgAbqQpm49vW/giphy.gif"
+            style={{
+              margin: "0 auto auto auto",
+              maxHeight: "40vh",
+              height: "40vh",
+              width: "auto",
+              objectFit: "cover",
+              borderRadius: "6px",
+              zIndex: 1000,
+            }}
+          />
+        </motion.div>
+      )}
     </Backdrop>
   );
 };
