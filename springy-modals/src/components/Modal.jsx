ed6b4b4e490719e42648064aaa07deff38695642
Harold Varde <harvardii15@gmail.com>
2023-01-01 23:23:00 +0800

    MOD: modal inner components + animate

--- springy-modals/src/components/Modal.jsx
+++ springy-modals/src/components/Modal.jsx
@@ -12,7 +12,7 @@ const dropIn = {
     transition: {
       duration: 0.1,
       type: "spring",
-      damping: 0,
+      damping: 25,
       stiffness: 500,
     },
   },
@@ -33,10 +33,33 @@ const Modal = ({ handleClose, text }) => {
         animate="visible"
         exit="exit"
       >
-        test
+        <ModalText text={text} />
+        <ModalButton onClick={handleClose} label="Close" />
       </motion.div>
     </Backdrop>
   );
+};
+
+const ModalText = ({ text }) => {
+  <div className="modal-text">
+    <h3>text</h3>
+    <h5>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi dolorem
+      iure praesentium error eligendi. Exercitationem dolores eligendi in
+      blanditiis, itaque ratione omnis.
+    </h5>
+  </div>;
+};
+
+const ModalButton = ({ onClick, label }) => {
+  <motion.button
+    className="modal-button"
+    type="button"
+    whileHover={{ scale: 1.125 }}
+    whileTap={{ scale: 0.95 }}
+  >
+    {label}
+  </motion.button>;
 };
 
 export default Modal;
