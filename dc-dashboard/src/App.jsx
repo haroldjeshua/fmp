9d69493e0a41bd60cb37d57e2ddab23a878d956f
Harold Varde <harvardii15@gmail.com>
2023-01-02 15:58:40 +0800

    MOD: content comp styling + class error

--- dc-dashboard/src/App.jsx
+++ dc-dashboard/src/App.jsx
@@ -1,12 +1,14 @@
 import Sidebar from "./components/Sidebar";
 import "./App.css";
 import ChannelBar from "./components/ChannelBar";
+import ContentContainer from "./components/ContentContainer";
 
 function App() {
   return (
     <div className="flex">
       <Sidebar />
       <ChannelBar />
+      <ContentContainer />
     </div>
   );
 }
--- dc-dashboard/src/components/ContentContainer.jsx
+++ dc-dashboard/src/components/ContentContainer.jsx
@@ -3,8 +3,62 @@ import { BsPlusCircleFill } from "react-icons/bs";
 const ContentContainer = () => {
   return (
     <div className="content-container">
-      <TopNavigation />
-      <div className="content-list">{/* Post Component */}</div>
+      {/* <TopNavigation /> */}
+      <div className="content-list">
+        <Post
+          name="Harold"
+          timestamp="5 days ago"
+          text={`Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati doloribus aspernatur, iusto ullam, alias distinctio consequuntur eaque modi nobis aperiam perferendis libero culpa fugiat dolorum officia voluptas ab ut tempora.`}
+        />
+        <Post
+          name="Leon"
+          timestamp="one week ago"
+          text={`Lorem ipsum dolor. `}
+        />
+        <Post name="Jill" timestamp="5 days ago" text={`Lorem.`} />
+        <Post
+          name="Ellie"
+          timestamp="4 days ago"
+          text={`Lorem ipsum dolor sit amet consectetur adipisicing elit. `}
+        />
+        <Post
+          name="Chris"
+          timestamp="4 days ago"
+          text={`Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit
+          amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem
+          ipsum dolor sit amet consectetur adipisicing elit.
+          
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem
+          ipsum dolor sit amet consectetur adipisicing elit.`}
+        />
+        <Post
+          name="Claire"
+          timestamp="2 days ago"
+          text={`Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit
+          amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. `}
+        />
+        <Post
+          name="Albert"
+          timestamp="22 hours ago"
+          text={`Lorem ipsum dolor sit amet consectetur adipisicing elit. ☺️ `}
+        />
+        <Post
+          name="Rebecca"
+          timestamp="3 hours ago"
+          text={`Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit
+          amet consectetur adipisicing elit.`}
+        />
+        <Post
+          name="H.U.N.K"
+          timestamp="Just now"
+          text={`Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit
+          amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem
+          ipsum dolor sit amet consectetur adipisicing elit.`}
+        />
+      </div>
       <BottomBar />
     </div>
   );
@@ -21,14 +75,13 @@ const Post = ({ name, timestamp, text }) => {
           alt="/"
           className="avatar"
         />
-
-        <div className="post-content">
-          <p className="post-author">
-            {name}
-            <small className="timestamp">{timestamp}</small>
-          </p>
-          <p className="post-text">{text}</p>
-        </div>
+      </div>
+      <div className="post-content">
+        <p className="post-author">
+          {name}
+          <small className="timestamp">{timestamp}</small>
+        </p>
+        <p className="post-text">{text}</p>
       </div>
     </div>
   );
