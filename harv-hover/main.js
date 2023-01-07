cfd8abab30eb611d780b4a7f6bda214a2c3a543a
Harold Varde <harvardii15@gmail.com>
2023-01-07 22:30:15 +0800

    MOD: add pickletter func

--- harv-hover/main.js
+++ harv-hover/main.js
@@ -1,1 +1,21 @@
 import "./style.css";
+
+const pickLetters = (id) => {
+  const element = document.querySelector(id);
+  const text = element.innerText.split("");
+
+  console.log(element);
+
+  element.innerText = "";
+
+  text.forEach((letter) => {
+    const span = document.createElement("span");
+
+    span.className = "letter";
+    span.innerText = letter;
+
+    element.appendChild(span);
+  });
+};
+
+pickLetters("#word-hover");
