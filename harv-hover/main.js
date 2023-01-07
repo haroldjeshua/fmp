2650632531d9f63d76aafecf4614ce538e99f4a7
Harold Varde <harvardii15@gmail.com>
2023-01-07 23:20:12 +0800

    MOD: enhancelinks

--- harv-hover/main.js
+++ harv-hover/main.js
@@ -4,9 +4,8 @@ const pickLetters = (id) => {
   const element = document.querySelector(id);
   const text = element.innerText.split("");
 
-  console.log(element);
-
   element.innerText = "";
+  console.log(element);
 
   text.forEach((letter) => {
     const span = document.createElement("span");
@@ -18,4 +17,36 @@ const pickLetters = (id) => {
   });
 };
 
-pickLetters("#word-hover");
+// pickLetters("#word-hover");
+
+const enhanceLinks = (ref) => {
+  const elements = document.querySelectorAll(ref);
+
+  elements.forEach((element) => {
+    const text = element.innerText.split("");
+    element.innerText = "";
+    console.log(element);
+
+    text.forEach((value, index) => {
+      const outer = document.createElement("span");
+      outer.className = "outer";
+
+      const inner = document.createElement("span");
+      inner.className = "inner";
+      inner.style.animationDelay = "250ms";
+
+      const letter = document.createElement("span");
+      letter.className = "letter";
+      letter.innerText = value;
+      letter.style.animationDelay = `${index * 1000}ms`;
+
+      inner.appendChild(letter);
+      outer.appendChild(inner);
+      element.appendChild(outer);
+    });
+  });
+
+  console.log(elements);
+};
+
+enhanceLinks(".word.link");
