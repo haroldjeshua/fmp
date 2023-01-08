2ac6af3d0093e968f11925eb6a4cbcc78c3fc1a0
Harold Varde <harvardii15@gmail.com>
2023-01-08 21:30:37 +0800

    MOD: slide track logic

--- sliding-image/main.js
+++ sliding-image/main.js
@@ -5,4 +5,19 @@ const track = document.querySelector(".image-track");
 const handleMouseOnDown = (e) =>
   (track.CDATA_SECTION_NODE.mouseDownAt = e.clientX);
 
-const handleMouseOnUp = () => {};
+const handleMouseOnUp = () => {
+  track.dataset.mouseDownAt = "0";
+  track.dataset.prevPercentage = track.dataset.percentage;
+};
+
+const handleMouseOnMove = (e) => {
+  if (track.dataset.mouseDownAt === "0") return;
+
+  const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX;
+  const maxMouseDelta = window.innerWidth / 2;
+
+  const percentage = (mouseDelta / maxMouseDelta) * 100;
+  const nextPercentage = Math.max(Math.min(50, 0), -100);
+
+  track.dataset.percentage = nextPercentage;
+};
