a620145a7566af744bdced407a366b5a3175849e
Harold Varde <harvardii15@gmail.com>
2023-01-08 21:45:09 +0800

    MOD: animate + window events

--- sliding-image/main.js
+++ sliding-image/main.js
@@ -2,8 +2,7 @@ import "./style.css";
 
 const track = document.querySelector(".image-track");
 
-const handleMouseOnDown = (e) =>
-  (track.CDATA_SECTION_NODE.mouseDownAt = e.clientX);
+const handleMouseOnDown = (e) => (track.dataset.mouseDownAt = e.clientX);
 
 const handleMouseOnUp = () => {
   track.dataset.mouseDownAt = "0";
@@ -20,4 +19,32 @@ const handleMouseOnMove = (e) => {
   const nextPercentage = Math.max(Math.min(50, 0), -100);
 
   track.dataset.percentage = nextPercentage;
+
+  track.animate(
+    {
+      transform: `translate(${nextPercentage}%, 50%)`,
+    },
+    { duration: 1200, fill: "forwards" }
+  );
+
+  for (const image of track.querySelectorAll(".image")) {
+    image.animate(
+      {
+        objectPosition: `${100 + nextPercentage}% center`,
+      },
+      { duration: 1200, fill: "forwards" }
+    );
+  }
 };
+
+window.onmousedown = (e) => handleMouseOnDown(e);
+
+window.ontouchstart = (e) => handleMouseOnDown(e.touches[0]);
+
+window.onmouseup = (e) => handleMouseOnUp(e);
+
+window.ontouchend = (e) => handleMouseOnUp(e.touches[0]);
+
+window.onmousemove = (e) => handleMouseOnMove(e);
+
+window.ontouchmove = (e) => (handleMouseOnMove = e.touches[0]);
