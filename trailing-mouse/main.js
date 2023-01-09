484901156ea815001a6a24ec902deda19fb87a5d
Harold Varde <harvardii15@gmail.com>
2023-01-10 00:20:46 +0800

    MOD: add icon on hover

--- trailing-mouse/main.js
+++ trailing-mouse/main.js
@@ -19,9 +19,23 @@ const animateTrailer = (e, interacting) => {
   });
 };
 
+const getTrailerClass = (type) => {
+  switch (type) {
+    case "video":
+      return "fa-solid fa-play";
+    default:
+      return "fa-solid fa-arrow-up-right-from-square";
+  }
+};
+
 window.onmousemove = (e) => {
   const interactable = e.target.closest(".interactable");
   const interacting = interactable !== null;
+  const icon = document.querySelector(".trailing-icon");
 
   animateTrailer(e, interacting);
+
+  if (interacting) {
+    icon.className = getTrailerClass(interactable.dataset.type);
+  }
 };
