413c9e15a223a16efc73f79a9006eba949f0a8ea
Harold Varde <harvardii15@gmail.com>
2023-01-09 23:33:52 +0800

    MOD: add anim on js

--- trailing-mouse/main.js
+++ trailing-mouse/main.js
@@ -3,8 +3,18 @@ import "./style.css";
 const trailer = document.querySelector(".trailing-mouse");
 
 window.onmousemove = (e) => {
-  const x = e.clientX;
-  const y = e.clientY;
+  const x = e.clientX - trailer.offsetWidth / 2;
+  const y = e.clientY - trailer.offsetHeight / 2;
 
-  trailer.style.transform = `translate(${x}px, ${y}px)`;
+  // trailer.style.transform = `translate(${x}px, ${y}px)`;
+
+  // web animation api
+  const keyframes = {
+    transform: `translate(${x}px, ${y}px)`,
+  };
+
+  trailer.animate(keyframes, {
+    duration: 400,
+    fill: "forwards",
+  });
 };
