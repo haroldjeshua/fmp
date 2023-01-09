39392fe2f2ed9cde0dad9c7f7290e9f2b4723995
Harold Varde <harvardii15@gmail.com>
2023-01-09 23:25:59 +0800

    ADD: render + transform on DOM

--- trailing-mouse/main.js
+++ trailing-mouse/main.js
@@ -1,1 +1,10 @@
 import "./style.css";
+
+const trailer = document.querySelector(".trailing-mouse");
+
+window.onmousemove = (e) => {
+  const x = e.clientX;
+  const y = e.clientY;
+
+  trailer.style.transform = `translate(${x}px, ${y}px)`;
+};
