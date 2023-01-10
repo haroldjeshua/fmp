85157e70b604647896e251d04c20295a6005426d
Harold Varde <harvardii15@gmail.com>
2023-01-10 23:38:45 +0800

    MOD: text center + animate canvas

--- moving-canvas/main.js
+++ moving-canvas/main.js
@@ -1,1 +1,28 @@
 import "./style.css";
+
+const canvas = document.querySelector(".canvas");
+
+window.onmousemove = (e) => {
+  const mouseX = e.clientX;
+  const mouseY = e.clientY;
+
+  const xDecimal = mouseX / window.innerWidth;
+  const yDecimal = mouseY / window.innerHeight;
+
+  const xMax = canvas.offsetWidth - window.innerWidth;
+  const yMax = canvas.offsetHeight - window.innerHeight;
+
+  const panX = xMax * xDecimal * -1;
+  const panY = yMax * yDecimal * -1;
+
+  canvas.animate(
+    {
+      transform: `translate(${panX}px, ${panY}px)`,
+    },
+    {
+      duration: 2000,
+      easing: "ease",
+      fill: "forwards",
+    }
+  );
+};
