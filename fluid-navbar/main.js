c6d2782ccf5177b454bd5fa603071e284a45f893
Harold Varde <harvardii15@gmail.com>
2023-01-12 01:36:12 +0800

    ADD: theme switcher on nav

--- fluid-navbar/main.js
+++ fluid-navbar/main.js
@@ -1,1 +1,24 @@
 import "./style.css";
+
+// theme switcher
+const themeMap = {
+  dark: "light",
+  light: "solar",
+  solar: "dark",
+};
+
+const theme =
+  localStorage.getItem("theme") ||
+  ((tmp = Object.keys(themeMap)[0]), localStorage.setItem("theme", tmp), tmp);
+const bodyClass = document.body.classList;
+bodyClass.add(theme);
+
+function toggleTheme() {
+  const current = localStorage.getItem("theme");
+  const next = themeMap[current];
+
+  bodyClass.replace(current, next);
+  localStorage.setItem("theme", next);
+}
+
+document.querySelector(".theme-button").onclick = toggleTheme;
