ab40e4fe3e7c2ac617e1ed59fa658d08de50a25f
Harold Varde <harvardii15@gmail.com>
2023-01-24 00:11:29 +0800

    Automated commit, build and deploy

--- card-scrolllink/main.js
+++ card-scrolllink/main.js
@@ -1,12 +1,54 @@
 import "./style.css";
 
-// "Nature"
-// "Cityscape"
-// "Landscape"
-// "Portrait"
-// "Sunset"
-// "Architecture"
-// "Abstract"
-// "Animal"
-// "Flowers"
-// "Food"
+const slides = [
+  {
+    title: "Nature",
+    background:
+      "https://images.unsplash.com/photo-1505144808419-1957a94ca61e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1526&q=80",
+  },
+  {
+    title: "Cityscape",
+    background:
+      "https://images.unsplash.com/photo-1550837725-7998bc8efdb0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80",
+  },
+  {
+    title: "Landscape",
+    background:
+      "https://images.unsplash.com/photo-1489549132488-d00b7eee80f1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80",
+  },
+  {
+    title: "Portrait",
+    background:
+      "https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1528&q=80",
+  },
+  {
+    title: "Sunset",
+    background:
+      "https://images.unsplash.com/photo-1585477281005-b83d4b47eba4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80",
+  },
+  {
+    title: "Architecture",
+    background:
+      "https://images.unsplash.com/photo-1479839672679-a46483c0e7c8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1420&q=80",
+  },
+  {
+    title: "Abstract",
+    background:
+      "https://images.unsplash.com/photo-1604076913837-52ab5629fba9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80",
+  },
+  {
+    title: "Animal",
+    background:
+      "https://images.unsplash.com/photo-1534759846116-5799c33ce22a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1500&q=80",
+  },
+  {
+    title: "Flowers",
+    background:
+      "https://images.unsplash.com/photo-1531131141161-ecdfb1858dd2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80",
+  },
+  {
+    title: "Food",
+    background:
+      "https://images.unsplash.com/photo-1611280422374-fa3c1110c16e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80",
+  },
+];
