b0f21dfdf0b9f471dc8efd680819353dfbec661f
Harold Varde <harvardii15@gmail.com>
2023-01-28 00:57:47 +0800

    Automated commit, build and deploy

--- tw-play/src/components/Cards.jsx
+++ tw-play/src/components/Cards.jsx
@@ -1,4 +1,15 @@
 const Cards = () => {
+  document.querySelector(".cards").onmousemove = (e) => {
+    for (const card of document.querySelectorAll(".card")) {
+      const rect = card.getBoundingClientRect();
+      const x = e.clientX - rect.left;
+      const y = e.clientY - rect.top;
+
+      card.style.setProperty("--mouse-x", `${x}px`);
+      card.style.setProperty("--mouse-y", `${y}px`);
+    }
+  };
+
   const cardData = [
     {
       icon: "123",
@@ -9,14 +20,14 @@ const Cards = () => {
   ];
 
   return (
-    <div className="cards grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+    <div className="cards w-screen max-w-xl mx-auto grid grid-cols-2 md:grid-cols-3 gap-4">
       {cardData.map((card, idx) => (
         <div key={idx} className="card">
           <div className="card-content">
-            <div className="card-icon">Card Icon</div>
-            <div className="card-text">
-              <h2>{card.heading}</h2>
-              <p>{card.subheading}</p>
+            <div className="card-icon mb-2">Card Icon</div>
+            <div className="flex flex-col gap-2">
+              <h2 className="text-lg leading-tight">{card.heading}</h2>
+              <p className="text-xs">{card.subheading}</p>
             </div>
           </div>
         </div>
--- tw-play/src/components/Header.jsx
+++ tw-play/src/components/Header.jsx
@@ -2,7 +2,7 @@ import { TbBrandTailwind } from "react-icons/tb";
 
 const Header = () => {
   return (
-    <div className="w-screen min-w-lg flex flex-col justify-center items-center">
+    <div className="w-screen max-w-xl mx-auto pb-8 flex flex-col justify-center items-center">
       <h1 className="text-5xl flex flex-col items-center gap-4">
         <TbBrandTailwind /> Tailwind Playground
       </h1>
