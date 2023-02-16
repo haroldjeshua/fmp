27350fedeb9ea1492a793c63a59c438249b35387
Harold Varde <harvardii15@gmail.com>
2023-02-16 22:00:34 +0800

    ADD:  new pen: harvstack

---
+++ harv-stack/main.js
@@ -0,0 +1,12 @@
+import "./style.css";
+
+document.querySelector(".cards").onmousemove = (e) => {
+  for (const card of document.querySelectorAll(".card")) {
+    const rect = card.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    card.style.setProperty("--mouse-x", `${x}px`);
+    card.style.setProperty("--mouse-y", `${y}px`);
+  }
+};
