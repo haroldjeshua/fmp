82ba04baa72abe26978670a77881e7e9dce1fa6d
Harold Varde <harvardii15@gmail.com>
2023-07-04 15:40:13 +0800

    new mp: gsap polish

--- harv-gsap/main.js
+++ harv-gsap/main.js
@@ -9,13 +9,13 @@ function startLoader() {
       return;
     }
 
-    counterElement += Math.floor(Math.random() * 10) + 1;
+    currentValue += Math.floor(Math.random() * 10) + 1;
 
     if (currentValue > 100) {
       currentValue = 100;
     }
 
-    currentValue.textContent = currentValue;
+    counterElement.textContent = currentValue;
 
     let delay = Math.floor(Math.random() * 200) + 50;
     setTimeout(updateCounter, delay);
@@ -29,6 +29,7 @@ startLoader();
 gsap.to(".counter", 0.25, {
   delay: 3.5,
   opacity: 0,
+  height: 0,
 });
 
 gsap.to(".bar", 1.5, {
@@ -40,13 +41,17 @@ gsap.to(".bar", 1.5, {
   ease: "power4.inOut",
 });
 
-gsap.to(".h1", 1.5, {
+gsap.from(".h1", 1.5, {
   delay: 4,
   y: 700,
   stagger: {
     amount: 0.5,
   },
   ease: "power4.inOut",
+});
+
+gsap.to(".overlay", 1.75, {
+  height: 0,
 });
 
 gsap.from("hero", 2, {
